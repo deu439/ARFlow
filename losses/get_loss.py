a9be3377d079d9cdd2fef18bef8b0b536309eb9b
Jan Dorazil <deu439@gmail.com>
2023-07-12 12:18:22 +0200

    Implement probabilistic PWCnet, Elbo loss...

--- losses/get_loss.py
+++ losses/get_loss.py
@@ -1,8 +1,11 @@
 from .flow_loss import unFlowLoss
+from .elbo_loss import ElboLoss
 
 def get_loss(cfg):
     if cfg.type == 'unflow':
         loss = unFlowLoss(cfg)
+    elif cfg.type == 'elbo':
+        loss = ElboLoss(cfg)
     else:
         raise NotImplementedError(cfg.type)
     return loss
