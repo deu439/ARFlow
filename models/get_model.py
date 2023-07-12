6835728c588ee2ec4a0c3ede34fc26c3c09ebe26
Jan Dorazil <deu439@gmail.com>
2023-07-12 12:42:56 +0200

    Fix validation with PWCnet prob..

--- models/get_model.py
+++ models/get_model.py
@@ -1,8 +1,12 @@
 from .pwclite import PWCLite
+from .pwclite_prob import PWCLiteProb
+
 
 def get_model(cfg):
     if cfg.type == 'pwclite':
         model = PWCLite(cfg)
+    elif cfg.type == 'pwclite_prob':
+        model = PWCLiteProb(cfg)
     else:
         raise NotImplementedError(cfg.type)
     return model
--- trainer/get_trainer.py
+++ trainer/get_trainer.py
@@ -1,6 +1,7 @@
 from . import sintel_trainer, sintel_trainer_ar
 from . import kitti_trainer, kitti_trainer_ar
 from . import chairs_trainer
+from . import chairs_elbo_trainer
 
 
 def get_trainer(name):
@@ -14,6 +15,8 @@ def get_trainer(name):
         TrainFramework = kitti_trainer_ar.TrainFramework
     elif name == 'Chairs':
         TrainFramework = chairs_trainer.TrainFramework
+    elif name == 'ChairsElbo':
+        TrainFramework = chairs_elbo_trainer.TrainFramework
     else:
         raise NotImplementedError(name)
 
